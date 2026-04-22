cfba84f6804a479171bca9ba85fbb6f70628a550
袁媛 <debbyyuan698@gmail.com>
2026-04-22 15:41:32 +0800

    Delete My Python Practice directory

--- My Python Practice/cybersecurity_1.py
+++
@@ -1,7 +0,0 @@
-from string import digits
-
-for i in digits:
-    for j in digits:
-        for k in digits:
-            for l in digits:
-                print(i, j, l, k)
--- My Python Practice/verify even & odd integers.py
+++
@@ -1,6 +0,0 @@
-num=int(input('Please input an integer:'))
-
-if num%2==0:
-    print(int(num),'is an even number')
-else:
-    print(int(num),'is an odd number.')
